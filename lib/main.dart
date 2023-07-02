bde456f3aaa431fc889b1ba4c1aa4fc61e5af0de
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-07-02 16:02:55 +0900

    register

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:my_chat_app/pages/register_page.dart';
 
 import 'constants.dart';
 
@@ -17,13 +18,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'チャットアプリ',
+      home: RegisterPage(),
     );
   }
 }
